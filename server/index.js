cc156eb37bcdda3472b17a8a122bfe71381169bd
fouadslh001 <salhanefouad@gmail.com>
2024-11-05 11:10:23 +0100

    update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-/* confugirations */
+/* Confugirations */
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
